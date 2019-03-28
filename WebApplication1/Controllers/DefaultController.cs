e9ba243c12c17ac78d09557848b45f8c5d5df3dc
Amadou Bailo BAH <coladaitp18-bam@ccicampus.fr>
2019-03-28 13:43:15 +0100

    modif

--- WebApplication1/Controllers/DefaultController.cs
+++ WebApplication1/Controllers/DefaultController.cs
@@ -8,7 +8,7 @@ namespace WebApplication1.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+        // GET: Default    //modific
         public ActionResult Index()
         {
             return View();
